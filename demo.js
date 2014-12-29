0fe74ad208db0a845a0228371859f82a7908b866
Bjorn Westergard <bjornwestergard@Bjorns-MacBook-Pro.local>
2014-12-29 01:10:50 -0500

    chart updates in response to scope changes
    Signed-off-by: Bjorn Westergard <bjornwestergard@Bjorns-MacBook-Pro.local>

--- demo.js
+++ demo.js
@@ -12,96 +12,95 @@ angular.
   module("ordnung").
   controller('main', ['$scope', function($scope) {
     $scope.tasks = example;
+    $scope.goal = 13;
   }]).
   directive("depchart", function () {
-    return {
-      restrict: 'E',
-      scope: true,
-      template: '<pre>{{ tasks | json:2 }}</pre>'
-      //    template: '<div><svg id="svg-canvas" width="1024" height="500"></svg></div>'
-    };
-  });
 
-/*
-
-function trace(id) {
-  return deps(example, id).concat([id]);
-}
-
-window.onload = function () {
-
-  // Create the renderer
-  var render = new dagreD3.render();
-
-  // Set up an SVG group so that we can translate the final graph.
-  var svg = d3.select("svg");
-  var svgGroup = svg.append("g");
-
-  // Set up zoom support
-  var zoom = d3.behavior.zoom().on("zoom", function() {
-    svgGroup.attr("transform", "translate(" + d3.event.translate + ")" +
-                  "scale(" + d3.event.scale + ")");
-  });
-  svg.call(zoom);
-
-  // Create the input graph
-  var g = new dagreD3.graphlib.Graph()
-    .setGraph({})
-    .setDefaultEdgeLabel(function() { return {}; });
-
-  g.graph().nodesep = 20;
-  g.graph().ranksep = 80;
-  g.graph().rankdir = 'BT';
-
-  g.graph().transition = function(selection) {
-    return selection.transition().duration(1000);
-  };
-
-  draw();
-
-  window.draw = draw;
-
-  function draw(id) {
-
-    g.nodes().forEach(function (node) {
-      g.removeNode(node);
-    });
-
-    // Here we're setting nodeclass, which is used by our custom drawNodes function
-    // below.
-
-    if (id) {
-      var depset = trace(id);
-      var filtered = _.filter(example, function (item) {
-        return _.contains(depset, item.id);
-        //    return item.tags.indexOf('russia') > -1;
-      });
-    } else {
-      var filtered = example;
+    function trace(tasks, goal) {
+      return deps(example, goal).concat([goal]);
     }
 
-    filtered.forEach(function (item) {
-      g.setNode(item.id, {
-        label: item.description + ' (' + item.id + ')'
+    var link = function ($scope, $el, $attrs) {
+
+      // Setup
+
+      // Create the renderer
+      var render = new dagreD3.render();
+
+      // Set up an SVG group so that we can translate the final graph.
+      var svg = d3.select("svg");
+      var svgGroup = svg.append("g");
+
+      // Set up zoom support
+      var zoom = d3.behavior.zoom().on("zoom", function() {
+        svgGroup.attr("transform", "translate(" + d3.event.translate + ")" +
+                      "scale(" + d3.event.scale + ")");
       });
-    });
+      svg.call(zoom);
 
-    var edges = _.flatten(_.map(filtered, function (item) {
-      return _.map(item.dependencies, function (dependency) {
-        return [item.id, dependency];
-      });
-    }), true);
+      // Create the input graph
+      var g = new dagreD3.graphlib.Graph()
+        .setGraph({})
+        .setDefaultEdgeLabel(function() { return {}; });
 
-    edges.forEach(function (edge) {
-      g.setEdge(edge[1], edge[0],
-                { lineInterpolate: 'linear' });
-    });
-    
-    // Run the renderer. This is what draws the final graph.
-    render(d3.select("svg g"), g);
+      g.graph().nodesep = 20;
+      g.graph().ranksep = 80;
+      g.graph().rankdir = 'BT';
 
-  }
+      g.graph().transition = function(selection) {
+        return selection.transition().duration(1000);
+      };
 
-};
+      $scope.$watch('tasks', update);
+      $scope.$watch('goal', update);
 
-*/
+      function update() {
+
+        g.nodes().forEach(function (node) {
+          g.removeNode(node);
+        });
+
+        // Here we're setting nodeclass, which is used by our custom drawNodes function
+        // below.
+
+        if ($scope.goal) {
+          var depset = trace($scope.tasks, $scope.goal);
+          var filtered = _.filter($scope.tasks, function (item) {
+            return _.contains(depset, item.id);
+            //    return item.tags.indexOf('russia') > -1;
+          });
+        } else {
+          var filtered = $scope.tasks;
+        }
+
+        filtered.forEach(function (item) {
+          g.setNode(item.id, {
+            label: item.description + ' (' + item.id + ')'
+          });
+        });
+
+        var edges = _.flatten(_.map(filtered, function (item) {
+          return _.map(item.dependencies, function (dependency) {
+            return [item.id, dependency];
+          });
+        }), true);
+
+        edges.forEach(function (edge) {
+          g.setEdge(edge[1], edge[0],
+                    { lineInterpolate: 'linear' });
+        });
+        
+        // Run the renderer. This is what draws the final graph.
+        render(d3.select("svg g"), g);
+
+      };
+
+    };
+
+    return {
+      restrict: 'E',
+      scope: '=',
+      link: link,
+      template: '<div> <p>Goal: {{ goal }}</p> <svg id="svg-canvas" width="2000" height="500"></svg></div>'
+    };
+  });
