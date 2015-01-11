9e04b5b3ae467e8148fb9f1a01aeee1f05186bd4
Bjorn Westergard <bjornwestergard@new-host.home>
2015-01-10 21:47:06 -0500

    forgotten files
    Signed-off-by: Bjorn Westergard <bjornwestergard@new-host.home>

---
+++ browser/lib/mainController.js
@@ -0,0 +1,26 @@
+var example = require('../example.json');
+
+module.exports = function($scope) {
+  $scope.tasks = example;
+  $scope.goal = null;
+
+  $scope.addDependency = function (task) {
+    var id = _.max(_.pluck($scope.tasks, 'id')) + 1;
+    $scope.tasks.push({
+      'id': id,
+      'description': 'New Item',
+      "dependencies": []
+    });
+    task.dependencies.push(id);
+  };
+
+  $scope.deleteTask = function (dead_task) {
+    $scope.tasks.forEach(function (task) {
+      _.pull(task.dependencies, dead_task.id);
+    });
+    $scope.tasks = _.filter($scope.tasks, function (task) {
+      return task.id != dead_task.id;
+    });
+  };
+
+}
