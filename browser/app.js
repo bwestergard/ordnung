966ac4a5125b3d7125f3315e3fdc11cb218836af
Bjorn Westergard <bjornw@gmail.com>
2015-02-07 22:29:52 -0500

    autocomplete sketch
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- browser/app.js
+++ browser/app.js
@@ -4,8 +4,9 @@ var ngEnter = require('./lib/ngEnter');
 var ngBackspace = require('./lib/ngBackspace');
 var depchart = require('./lib/depchart');
 var initFocus = require('./lib/initFocus');
+var autocomplete = require('allmighty-autocomplete');
 
-angular.module('ordnung', ['ng-sortable']).
+angular.module('ordnung', ['ng-sortable', 'autocomplete']).
   controller('main', ['$scope', mainController]).
   directive('ngEnter', ngEnter).
   directive('ngBackspace', ngBackspace).
